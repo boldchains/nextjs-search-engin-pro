085ca1b385bc0ce4071a86705533a32cf88a1de8
Svatosalv <stardustdev@outlook.com>
2020-01-29 07:33:47 +0900

    append images reducer

--- services/reducers/index.js
+++ services/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import articlesReducer from './articles/reducer';
 import videosReducer from './videos/reducer';
+import imagesReducer from './images/reducer';
 
 export default combineReducers({
     articles: articlesReducer,
-    videos: videosReducer
+    videos: videosReducer,
+    images: imagesReducer
 });
