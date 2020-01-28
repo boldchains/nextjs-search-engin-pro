699b0bdebe6df19d32655054159c1b8b26726903
Svatosalv <stardustdev@outlook.com>
2020-01-28 13:12:22 +0900

    update videos action types

--- services/reducers/videos/actionTypes.js
+++ services/reducers/videos/actionTypes.js
@@ -1,4 +1,4 @@
-export const SET_LANGUAGE = 'SET_LANGUAGE';
-export const ADD_ARTICLES = 'ADD_ARTICLES';
-export const CLEAR_ARTICLES = 'CLEAR_ARTICLES';
+export const SEARCH_VIDEOS = 'SEARCH_VIDEOS';
+export const CLEAR_VIDEOS = 'CLEAR_VIDEOS';
 export const SET_SEARCH_KEY = 'SET_SEARCH_KEY';
+export const SET_IS_FETCHING = 'SET_IS_FETCHING';
