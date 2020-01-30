04b87cd8460db7057a74dc7215fd0143353fc869
Svatosalv <stardustdev@outlook.com>
2020-01-30 16:20:47 +0900

    language resource bunch add

---
+++ assets/img/lang/index.js
@@ -0,0 +1,153 @@
+import ar from "./ar.png";
+import ca from "./ca.png";
+import de from "./de.png";
+import el from "./el.png";
+import en from "./en.png";
+import es from "./es.png";
+import fr from "./fr.png";
+import he from "./he.png";
+import id from "./id.png";
+import it from "./it.png";
+import ja from "./ja.png";
+import nl from "./nl.png";
+import pl from "./pl.png";
+import pt from "./pt.png";
+import ru from "./ru.png";
+import se from "./se.png";
+import th from "./th.png";
+import tr from "./tr.png";
+import zh from "./zh.png";
+import mini_ar from "./mini_ar.png";
+import mini_ca from "./mini_ca.png";
+import mini_de from "./mini_de.png";
+import mini_el from "./mini_el.png";
+import mini_en from "./mini_en.png";
+import mini_es from "./mini_es.png";
+import mini_fr from "./mini_fr.png";
+import mini_he from "./mini_he.png";
+import mini_id from "./mini_id.png";
+import mini_it from "./mini_it.png";
+import mini_ja from "./mini_ja.png";
+import mini_nl from "./mini_nl.png";
+import mini_pl from "./mini_pl.png";
+import mini_pt from "./mini_pt.png";
+import mini_ru from "./mini_ru.png";
+import mini_th from "./mini_th.png";
+import mini_tr from "./mini_tr.png";
+import mini_zh from "./mini_zh.png";
+
+// const languages = [
+//   {
+//     icon: ar,
+//     iconMini: mini_ar,
+//     language: "Arabic"
+//   },
+//   {
+//     icon: ca,
+//     iconMini: mini_ca,
+//     language: "Català"
+//   },
+//   {
+//     icon: nl,
+//     iconMini: mini_nl,
+//     language: "Dutch"
+//   },
+//   {
+//     icon: es,
+//     iconMini: mini_es,
+//     language: "Español"
+//   },
+//   {
+//     icon: en,
+//     iconMini: mini_en,
+//     language: "English"
+//   },
+//   {
+//     icon: fr,
+//     iconMini: mini_fr,
+//     language: "French"
+//   },
+//   {
+//     icon: de,
+//     iconMini: mini_de,
+//     language: "German"
+//   },
+//   {
+//     icon: el,
+//     iconMini: mini_el,
+//     language: "Greek"
+//   },
+//   {
+//     icon: he,
+//     iconMini: mini_he,
+//     language: "Hebrew"
+//   },
+//   {
+//     icon: id,
+//     iconMini: mini_id,
+//     language: "Indonesian"
+//   },
+//   {
+//     icon: it,
+//     iconMini: mini_it,
+//     language: "Italiano"
+//   },
+//   {
+//     icon: ja,
+//     iconMini: mini_ja,
+//     language: "Japan"
+//   },
+//   {
+//     icon: pl,
+//     iconMini: mini_pl,
+//     language: "Polish"
+//   },
+//   {
+//     icon: pt,
+//     iconMini: mini_pt,
+//     language: "Portuguese"
+//   },
+//   {
+//     icon: ru,
+//     iconMini: mini_ru,
+//     language: "Russian"
+//   },
+//   {
+//     icon: th,
+//     iconMini: mini_th,
+//     language: "Thai"
+//   },
+//   {
+//     icon: tr,
+//     iconMini: mini_tr,
+//     language: "Turkish"
+//   },
+//   {
+//     icon: zh,
+//     iconMini: mini_zh,
+//     language: "Chinese"
+//   }
+// ];
+
+const languages = [
+  "Español",
+  "Arabic",
+  "Català",
+  "Chinese",
+  "Dutch",
+  "English",
+  "French",
+  "German",
+  "Greek",
+  "Hebrew",
+  "Indonesian",
+  "Italiano",
+  "Japan",
+  "Polish",
+  "Portuguese",
+  "Russian",
+  "Thai",
+  "Turkis"
+];
+
+export default languages;
