d72bf37ceb00fe22163fb274d37f436cc4dc78e2
Svatosalv <stardustdev@outlook.com>
2020-02-02 22:33:51 +0900

    add location detection api to consts.js

--- utils/Consts.js
+++ utils/Consts.js
@@ -1,3 +1,4 @@
+export const LOCATION_DETECT_API = "https://extreme-ip-lookup.com/json/";
 export const TAGS_API_URL = "https://managerimages.vavel.com/api/tag/listtags";
 export const VIDEOS_API_URL = "https://www.googleapis.com/youtube/v3/search";
 export const IMAGES_API_URL =
