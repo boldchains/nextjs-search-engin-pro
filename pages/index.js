d689cfd40ae3fcc76d101a89990d0434c9079d54
Svatosalv <stardustdev@outlook.com>
2020-02-03 02:48:10 +0900

    modify index page for redux rebase

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,75 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { store } from "services/store.js";
-import Home from "./home.js";
+import React, { useEffect } from "react";
+import { connect, useDispatch, useSelector } from "react-redux";
+import { useRouter, withRouter } from "next/router";
+
+import Layout from "components/Layout/Layout.js";
+import CategoriesSection from "page-sections/home-sections/CategoriesSection.js";
+import ArticlesSection from "page-sections/home-sections/ArticlesSection.js";
+
+import {
+  setAllTags,
+  clearArticles,
+  setSearchKey
+} from "services/reducers/search/actions.js";
+
+import {
+  DEFAULT_COUNTRY_CODE,
+  TAGS_API_URL,
+  PHOTO_STATIC_URL
+} from "utils/Consts.js";
 
 const Index = () => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const location = useSelector(state => state.searchStates.location);
+  const countryCode =
+    location && location.countryCode
+      ? location.countryCode.toLowerCase()
+      : DEFAULT_COUNTRY_CODE;
+
+  useEffect(() => {
+    const query = router.query;
+    if (!query.l) {
+      router.push({
+        pathname: "/",
+        query: {
+          ...query,
+          l: countryCode
+        }
+      });
+    }
+
+    dispatch(clearArticles());
+    dispatch(setSearchKey(query.q ? query.q : ""));
+  }, [router.query.q]);
+
   return (
-    <Provider store={store}>
-      <Home />
-    </Provider>
+    <Layout>
+      <CategoriesSection />
+      <ArticlesSection />
+    </Layout>
   );
 };
 
-export default Index;
+Index.getInitialProps = async function({ store }) {
+  // fetch all tag list
+  const resp = await fetch(TAGS_API_URL);
+  const tags = await resp.json();
+
+  tags.listtags = tags.listtags || [];
+  const tagList = tags.listtags.map(item => {
+    if (item.tag && item.tag.logo) item.logo = PHOTO_STATIC_URL + item.tag.logo;
+    else {
+      let find = item.list_tags.find(i => i.logo);
+      if (find) {
+        item.logo = PHOTO_STATIC_URL + find.logo;
+      }
+    }
+    item.selected = false;
+    return item;
+  });
+
+  store.dispatch(setAllTags(tagList));
+};
+
+export default connect(state => state)(withRouter(Index));
