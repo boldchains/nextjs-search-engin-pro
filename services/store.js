1b7be071e147d1cd0c05335c438f89f28197c7eb
Svatosalv <stardustdev@outlook.com>
2020-02-03 19:34:03 +0900

    add thunck middleware for devtools

--- services/store.js
+++ services/store.js
@@ -1,6 +1,12 @@
 import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 export const makeStore = (initialState, options) => {
-  return createStore(rootReducer, initialState);
+  return createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(thunk))
+  );
 };
