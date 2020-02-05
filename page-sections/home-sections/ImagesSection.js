a94e0ff8a64973dbc529df80201ad58b6fea0cc9
Svatosalv <stardustdev@outlook.com>
2020-02-05 21:13:16 +0900

    remove tag filtering from imagesSection

--- page-sections/home-sections/ImagesSection.js
+++ page-sections/home-sections/ImagesSection.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
 
 import { makeStyles } from "@material-ui/core";
 
@@ -7,59 +6,17 @@ import Slider from "react-slick";
 import Card from "components/Card/Card.js";
 import ImageCard from "components/ImageCard/ImageCard.js";
 
-import { getImages } from "services/reducers/search/actions.js";
-
 import styles from "assets/jss/page-sections/home-sections/imagesSectionStyle.js";
 
 const useStyles = makeStyles(styles);
 
-const useFetchImages = () => {
-  const categoryTags = useSelector(state => state.searchStates.categoryTags);
-  const images = useSelector(state => state.searchStates.images);
-  const searchKey = useSelector(state => state.searchStates.searchKey);
-  const selectedCTag = useSelector(state => state.searchStates.selectedCTag);
-
-  const dispatch = useDispatch();
-
-  const searchHandler = () => {
-    const keyword =
-      selectedCTag && selectedCTag !== "" ? selectedCTag : searchKey;
-    const findTags = categoryTags.find(item => {
-      return (
-        item._id.toLowerCase().indexOf(keyword && keyword.toLowerCase()) >= 0
-      );
-    });
-
-    if (findTags && findTags.list_tags) {
-      const tagImages = findTags.list_tags.slice(0, 20);
-      dispatch(getImages(tagImages));
-    }
-  };
-
-  useEffect(() => {
-    searchHandler();
-  }, [searchKey, selectedCTag]);
-
-  return {
-    data: images
-  };
-};
-
-const ImagesSection = () => {
+const ImagesSection = props => {
   const classes = useStyles();
-  const images = useFetchImages();
+  const images = props.images;
 
-  const maxHeight = 120;
   const imageCards =
-    images.data &&
-    images.data.map((image, index) => {
-      if (image.height != maxHeight) {
-        const rate = maxHeight / image.height;
-        image.width = image.width * rate;
-        image.height = maxHeight;
-      }
-      return <ImageCard info={image} key={index} />;
-    });
+    images &&
+    images.map((image, index) => <ImageCard {...image} key={index} />);
 
   const settings = {
     className: "slider variable-width",
@@ -73,7 +30,7 @@ const ImagesSection = () => {
 
   return (
     <div>
-      {images.data && images.data.length !== 0 && (
+      {images && images.length !== 0 && (
         <Card className={classes.boxedCard}>
           <div className={classes.title}>Images</div>
           <div className={classes.imagesBox}>
