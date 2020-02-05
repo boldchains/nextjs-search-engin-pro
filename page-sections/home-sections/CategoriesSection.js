fb658503dbe05050f73bb37c5dc8d1741bc551e1
Svatosalv <stardustdev@outlook.com>
2020-02-05 22:13:51 +0900

    add getting favourite tags form photo list

--- page-sections/home-sections/CategoriesSection.js
+++ page-sections/home-sections/CategoriesSection.js
@@ -1,67 +1,98 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
 
 import { makeStyles } from "@material-ui/core";
 
 import Tags from "components/Tags/Tags.js";
 import ImagesSection from "./ImagesSection.js";
 import VideosSection from "./VideosSection.js";
-import { setCategoryTag } from "services/reducers/search/actions.js";
+
+import { COUNTRY_LIST } from "utils/Consts.js";
 
 import styles from "assets/jss/page-sections/home-sections/categoriesSectionStyle.js";
 
 const useStyles = makeStyles(styles);
 
-const useFetchTags = () => {
-  const allTags = useSelector(state => state.searchStates.allTags);
-  const tags = useSelector(state => state.searchStates.categoryTags);
-  const searchKey = useSelector(state => state.searchStates.searchKey);
+const getTags = function({ list, query }) {
+  let tags = [];
+  let array = [];
+  const country_filter = query.l ? query.l : "";
 
-  const dispatch = useDispatch();
-
-  const searchHandler = () => {
-    const filteredList = allTags.filter(function(item) {
-      return item._id.toLowerCase().indexOf(searchKey) >= 0;
-    });
-    const tagList = allTags ? filteredList : [];
-    dispatch(setCategoryTags(tagList));
-  };
-
-  useEffect(() => {
-    searchKey && searchKey !== "" && searchHandler();
-  }, [searchKey]);
-
-  const onSelect = index => {
-    const clonedTags = [...tags];
-    clonedTags &&
-      clonedTags.map((item, idx) => {
-        item.selected = idx === index;
+  list.forEach(photo => {
+    if (photo.images.tags) {
+      photo.images.tags = photo.images.tags.map(e => {
+        e.country = (photo.albumcountry || "").toLowerCase();
+        return e;
       });
-
-    const selectedTag = clonedTags[index];
-    if (selectedTag && selectedTag._id !== "All") {
-      dispatch(setCategoryTag(selectedTag._id));
+      tags = tags.concat(photo.images.tags);
     }
+  });
 
-    clonedTags.shift();
-    dispatch(setCategoryTags(clonedTags));
-  };
+  tags.forEach(e => {
+    if (array.find(a => a.tag === e.tag)) {
+      array.find(a => a.tag === e.tag).count++;
+      if (e.country)
+        array.find(a => a.tag === e.tag).country.push(e.country.toLowerCase());
+    } else {
+      let json = {
+        tag: e.tag,
+        count: 1,
+        country: [],
+        class: query.tag === e.tag ? "active" : ""
+      };
+      if (e.country) json.country = [e.country.toLowerCase()];
+      array.push(json);
+    }
+  });
+  array = array.sort(function(a, b) {
+    return a.count < b.count ? 1 : -1;
+  });
+  if (country_filter) {
+    let paramcountry = country_filter.toLowerCase();
+    let country = COUNTRY_LIST.find(function(e) {
+      if (
+        e.name.toLowerCase() === paramcountry ||
+        e.alpha3code.toLowerCase() === paramcountry ||
+        e.alpha2code.toLowerCase() === paramcountry
+      ) {
+        return e;
+      }
+    });
+    if (country) {
+      let after = [],
+        before = [];
+      array.forEach(e => {
+        if (
+          e.country.find(
+            i =>
+              i === country.name.toLowerCase() ||
+              i === country.alpha3code.toLowerCase() ||
+              i === country.alpha2code.toLowerCase()
+          )
+        ) {
+          before.push(e);
+        } else {
+          after.push(e);
+        }
+      });
+      array = before.concat(after);
+    }
+  }
 
-  return {
-    data: tags,
-    onSelect: onSelect
-  };
+  return array;
 };
 
-const CategoriesSection = () => {
+const CategoriesSection = props => {
   const classes = useStyles();
-  const categoryTags = useFetchTags();
+
+  const lastPhotos = props.searchStates.images;
+  const query = props.router.query;
+  const tags = getTags({ list: lastPhotos, query });
 
   return (
     <div className={classes.container}>
-      {/* <Tags tags={categoryTags} /> */}
+      <Tags tags={tags} />
       {/* <VideosSection /> */}
-      <ImagesSection />
+      <ImagesSection images={lastPhotos} />
     </div>
   );
 };
